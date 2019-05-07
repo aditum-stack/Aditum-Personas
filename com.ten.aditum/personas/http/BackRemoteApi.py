baa5ddfd5db8fb838cf7a71379ec32a50a34c6e4
Kevin_T <596823919@qq.com>
2019-05-07 21:48:17 +0800

    add : add api

---
+++ com.ten.aditum/personas/http/BackRemoteApi.py
@@ -0,0 +1,53 @@
+from ..entity.Person import Person
+
+import json
+import requests
+
+personUrl = "http://localhost:9006/person"
+recordUrl = "http://localhost:9006/record"
+
+headers = {'content-type': 'application/json'}
+requestData = {}
+
+
+def getForAllPerson():
+    """
+    获取所有person
+    :return: person[]
+    """
+    return requests.get(personUrl, params={}, headers=headers).json().get("data")
+
+
+def getForAllRecord():
+    """
+    获取所有record
+    :return: record[]
+    """
+    return requests.get(recordUrl, params={}, headers=headers).json().get("data")
+
+
+def getForRecordByPersonId(personId):
+    """
+    获取指定person的所有record
+    :param personId: personId
+    :return: record[]
+    """
+    request = {"personnelId": personId}
+    return requests.get(recordUrl, params=request, headers=headers).json().get("data")
+
+
+personList = getForAllPerson()
+recordList = getForAllRecord()
+
+for person in personList:
+    print(person)
+
+    personEntity = Person(person.get("id"))
+
+    recordByPersonId = getForRecordByPersonId(person.get("personnelId"))
+
+    for record in recordByPersonId:
+        print(record)
+
+# print(getForAllPerson())
+# print(json.loads(getForPerson(requestData).text).get("data")[0])
